1567d8ec44148ed497b4d0e724f628a5c35ac072
Charbel Antouny <charbel.antouny@gmail.com>
2015-10-06 23:06:56 +1100

    Completed step 1 (initialisation) and began working on step 2 (sending and receiving ping requests / responses).

--- src/com/charbelantouny/cdht.java
+++ src/com/charbelantouny/cdht.java
@@ -1,14 +1,46 @@
 package com.charbelantouny;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 /**
  * Copyright 2015 Charbel Antouny
  * This is a circular DHT network for the COMP3331 assignment.
  */
 public class cdht {
 
-    public static void main(String[] args) {
-	    int identity = Integer.parseInt(args[0]);
-        int peer1 = Integer.parseInt(args[1]);
-        int peer2 = Integer.parseInt(args[2]);
+    public static void main(String[] args) throws Exception {
+	    if (args.length != 3) {
+            System.out.println("3 arguments required.");
+            return;
+        }
+        int peer = Integer.parseInt(args[0]);
+        int suc1 = Integer.parseInt(args[1]);
+        int suc2 = Integer.parseInt(args[2]);
+
+        // Create a socket for sending and receiving UDP packets.
+        DatagramSocket socket = new DatagramSocket(50000+peer);
+
+        while (true) {
+            // Create a packet to receive a request.
+            DatagramPacket request = new DatagramPacket(new byte[1024], 1024);
+            // Wait for a packet to be received.
+            socket.receive(request);
+            // Print a message when a packet is received.
+            printInfo(request);
+
+            // Implement some kind of delay here using Thread.sleep (ms).
+
+            // Send a response.
+
+            // Also need code for receiving a response.
+            // A ping request message was received from Peer 5.
+        }
+    }
+
+    private static void printInfo (DatagramPacket request) throws Exception {
+        // A ping request message was received from Peer 5.
+
+        // http://www.java2s.com/Code/Java/Network-Protocol/UseDatagramSockettosendoutandreceiveDatagramPacket.htm
     }
 }

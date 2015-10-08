a823b50d7cf69857803e068b872399544d8828c2
Charbel Antouny <charbel.antouny@gmail.com>
2015-10-08 16:06:19 +1100

    Fixed many errors in step 2, and implemented a separate thread for receiving responses. The main thread now only sends requests. Step 2 now works correctly.

--- src/com/charbelantouny/cdht.java
+++ src/com/charbelantouny/cdht.java
@@ -1,8 +1,11 @@
 package com.charbelantouny;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Copyright 2015 Charbel Antouny
@@ -21,51 +24,74 @@ public class cdht {
 
         // Create a socket for sending and receiving UDP packets.
         DatagramSocket socket = new DatagramSocket(50000+peer);
+
+        // Create a separate thread to continuously listen for incoming packets
+        final DatagramSocket sock = socket;
+        final int refPeer = peer;
+
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    // Create a packet to receive a request.
+                    DatagramPacket dgp = new DatagramPacket(new byte[1024], 1024);
+                    // Wait for a packet to be received.
+                    try {
+                        sock.receive(dgp);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    // Print a message when a packet is received.
+                    try {
+                        printInfo(dgp,sock, refPeer);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        t.start();
         // Pause execution before begin sending and receiving (for visual benefit)
         Thread.sleep(10000);
 
         while (true) {
             // Send a ping request to successors
-            sendRequest(socket, suc1);
-            Thread.sleep(5000);
-            sendRequest(socket, suc2);
+            sendRequest(socket, peer, suc1);
+            sendRequest(socket, peer, suc2);
 
-            // Create a packet to receive a request.
-            DatagramPacket dgp = new DatagramPacket(new byte[1024], 1024);
-            // Wait for a packet to be received.
-            socket.receive(dgp);
-            // Print a message when a packet is received.
-            printInfo(dgp,socket);
-
-            // Wait 15 seconds before each ping request.
-            Thread.sleep(15000);
+            // Delay before sending another ping request.
+            Thread.sleep(20000);
         }
     }
 
-    private static void printInfo (DatagramPacket dgp, DatagramSocket s) throws Exception {
+    private static void printInfo (DatagramPacket dgp, DatagramSocket s, int peer) throws Exception {
         String line = new String(dgp.getData(), 0, dgp.getLength());
         System.out.println(line);
         if (line.contains("request")) {
-            sendResponse(s, dgp);
+            Pattern p = Pattern.compile(".*?(\\d+).*");
+            Matcher m = p.matcher(line);
+            m.matches();
+            String id = m.group(1);
+            sendResponse(s, dgp, id, peer);
         }
     }
 
     // method for sending request
-    private static void sendRequest (DatagramSocket s, int suc) throws Exception {
+    private static void sendRequest (DatagramSocket s, int peer, int suc) throws Exception {
         byte[] buf;
-        String message = "A ping request message was received from Peer " + suc + ".";
+        String message = "A ping request message was received from Peer " + peer + ".";
         buf = message.getBytes();
         DatagramPacket req = new DatagramPacket(buf, buf.length, InetAddress.getLocalHost(), 50000+suc);
         s.send(req);
     }
 
     // method for sending response
-    private static void sendResponse (DatagramSocket s, DatagramPacket dgp) throws Exception {
+    private static void sendResponse (DatagramSocket s, DatagramPacket dgp, String id, int peer) throws Exception {
         byte[] buf;
-        int peer = dgp.getPort() - 50000;
+        //int peer = dgp.getPort() - 50000;
         String message = "A ping response message was received from Peer " + peer + ".";
         buf = message.getBytes();
-        DatagramPacket res = new DatagramPacket(buf, buf.length, dgp.getAddress(), dgp.getPort());
+        DatagramPacket res = new DatagramPacket(buf, buf.length, dgp.getAddress(), 50000+Integer.parseInt(id));
         s.send(res);
     }
 }

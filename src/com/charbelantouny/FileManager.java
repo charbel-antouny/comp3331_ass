2450518beb0be289c961e9dd71d9060e12caccce
Charbel Antouny <charbel.antouny@gmail.com>
2015-10-14 15:05:55 +1100

    Created the FileManger class to handle file requests and responses using TCP.

---
+++ src/com/charbelantouny/FileManager.java
@@ -0,0 +1,12 @@
+package com.charbelantouny;
+
+/**
+ * Created by Charbel Antouny.
+ * This class manages file requests and responses.
+ */
+public class FileManager {
+
+    public void sendReceive () {
+        // TODO
+    }
+}
